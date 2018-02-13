711e043429d65ebb4456fce4c9c2f8ea61bb7a9c
mitcaizi <jling1@mail.csuchico.edu>
2018-02-12 17:42:46 -0800

    part1

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-//Authors:
+//Authors:Jingwen Ling
 #include<iostream>
 #include<string>
 
@@ -7,8 +7,18 @@ using namespace std;
 int main()
 {
   //declare variables
+   string firstname;
+   string lastname;
 
   //get user input
+cout<<"what is your first name?";
+cin>>firstname;
+cout<<"your fist name is:"<< firstname <<endl;
+cout <<"what is your last name?";
+cin>>lastname;
+cout<<"your last name is:"<< lastname <<endl;
+
+
 
   //tell fortune
 
