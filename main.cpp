7f4de4fe2fee0fcae318ca84dd9d3cb218c73231
mitcaizi <jling1@mail.csuchico.edu>
2018-02-14 21:57:04 -0800

    finalversion

--- main.cpp
+++ main.cpp
@@ -14,17 +14,18 @@ int main()
   //get user input
 cout<<"what is your first name?";
 cin>>firstname;
-cout<<"your fist name is:"<< firstname <<endl;
+//cout<<"your fist name is:"<< firstname <<endl;
 cout <<"what is your last name?";
 cin>>lastname;
-cout<<"your last name is:"<< lastname <<endl;
+//cout<<"your last name is:"<< lastname <<endl;
 
 string nickname;
 nickname.push_back(firstname[0]);
-nickname.push_back(',');
+nickname.push_back('.');
 nickname.push_back(lastname[0]);
+nickname.push_back('.');
 nickname.push_back(',');
-cout<<"Welcome,"<<nickname<<"here is your fortune..."<<endl;
+cout<<"Welcome, "<<nickname<<" here is your fortune..."<<endl;
 
 
   //tell fortune
@@ -38,7 +39,7 @@ cout<<"you are destined to be famous";
 }
 else
 {
-cout<<"you should keep a low profile"<<endl;
+cout<<"you should keep a low profile."<<endl;
 }
 if (lastname[len-1]=='a'||lastname[len-1]=='e'||lastname[len-1]=='i'||lastname[len-1]=='o'||lastname[0]=='u')
 {
@@ -48,7 +49,7 @@ else
 {
 }
 
-cout<<"have a good day"<<endl;
+cout<<"have a good day!"<<endl;
 
   return 0;
 }
