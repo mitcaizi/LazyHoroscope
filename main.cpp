8ca763b25784ceb437cb2da0391e204a4faa8947
mitcaizi <jling1@mail.csuchico.edu>
2018-02-14 21:42:34 -0800

    finalversion

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@ int main()
   //declare variables
    string firstname;
    string lastname;
+   
 
   //get user input
 cout<<"what is your first name?";
@@ -27,6 +28,27 @@ cout<<"Welcome,"<<nickname<<"here is your fortune..."<<endl;
 
 
   //tell fortune
+string vowel;
+cout<<"your lucky number is "<<firstname.length()<<endl;
+int len=lastname.length();
+if (firstname[0]=='a'||firstname[0]=='e'||firstname[0]=='i'||firstname[0]=='o'||firstname[0]=='u'||firstname[0]=='A'||
+firstname[0]=='E'||firstname[0]=='I'||firstname[0]=='O'||firstname[0]=='U')
+{
+cout<<"you are destined to be famous";
+}
+else
+{
+cout<<"you should keep a low profile"<<endl;
+}
+if (lastname[len-1]=='a'||lastname[len-1]=='e'||lastname[len-1]=='i'||lastname[len-1]=='o'||lastname[0]=='u')
+{
+cout<<"you already met your true love";
+}
+else
+{
+}
+
+cout<<"have a good day"<<endl;
 
   return 0;
 }
